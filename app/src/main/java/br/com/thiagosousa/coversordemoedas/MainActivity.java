6b50d7fd0d7043335d507e76984088c69d12067d
Thiago Sousa <thyagosousasilva@gmail.com>
2019-01-02 11:09:37 -0200

    Removendo imports não utilizados

--- app/src/main/java/br/com/thiagosousa/coversordemoedas/MainActivity.java
+++ app/src/main/java/br/com/thiagosousa/coversordemoedas/MainActivity.java
@@ -6,11 +6,9 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AnticipateOvershootInterpolator;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
